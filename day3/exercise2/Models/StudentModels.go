0b3e30c3d07248ed97b5ed451c24f552c6cef123
raajithapotala <87758012+raajithapotala@users.noreply.github.com>
2021-07-26 16:17:26 +0530

    Adding day3 files

---
+++ day3/exercise2/Models/StudentModels.go
@@ -0,0 +1,14 @@
+//Models/UserModel.go
+package Models
+type User struct {
+	Id      uint   `json:"id"`
+	Name    string `json:"name"`
+	LastName string `json: "lastName"`
+	DOB      string ` json: "DOB"`
+	Address string `json:"address"`
+	Subject   string `json:"subject"`
+	Marks    int `json:"marks"`
+}
+func (b *User) TableName() string {
+	return "user"
+}
